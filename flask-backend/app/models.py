c1b646552893c888cb7eae6726b5d56a3503f3e3
AJrealSFU <ajaxschoolid@gmail.com>
2025-10-04 14:26:43 -0700

    user data model

--- flask-backend/app/models.py
+++ flask-backend/app/models.py
@@ -5,10 +5,31 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    # Profile information
+    age = db.Column(db.Integer, nullable=True)
+    weight = db.Column(db.Float, nullable=True) # in kg
+    height = db.Column(db.Float, nullable=True) # in cm
+    gender = db.Column(db.String(1), nullable=True) # 'M' or 'F'
+    activity_level = db.Column(db.String(50), nullable=True) # e.g., 'Sedentary'
+
+    # Calculated values
+    tdee = db.Column(db.Float, nullable=True) # Total Daily Energy Expenditure
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def to_dict(self):
+        """Serializes the user object to a dictionary."""
+        return {
+            'id': self.id,
+            'username': self.username,
+            'age': self.age,
+            'weight': self.weight,
+            'height': self.height,
+            'gender': self.gender,
+            'activity_level': self.activity_level,
+            'tdee': self.tdee
+        }
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
