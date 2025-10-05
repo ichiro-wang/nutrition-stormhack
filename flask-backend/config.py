a709aae141bb1d776a9dbfe3b32e66732a5fd3e6
Patrina <92826777+SmolBeanPatrina@users.noreply.github.com>
2025-10-04 18:31:43 -0700

    Database Syncing issues resolved

--- flask-backend/config.py
+++ flask-backend/config.py
@@ -1,7 +1,11 @@
 class Config:
     SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_kvKnxM7DN6ie@ep-floral-scene-adyw3pzr-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+        "pool_recycle": 300
+    }
 
     @staticmethod
     def init_app(app):
